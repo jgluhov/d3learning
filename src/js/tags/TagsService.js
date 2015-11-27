beb16cade9bb0b697f561f8914bc1b3b0bd44fbb
jgluhov <jgluhov@gmail.com>
2015-11-27 14:04:50 +0300

    add input to the center.

--- src/js/tags/TagsService.js
+++ src/js/tags/TagsService.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.service('tagsService', ['$http', 'constants', function ($http, constants) {
     var self = this;
     self.getData = function() {
-      return $http.get(constants.production + 'emotions?cloud=true&lang_code=es&token=' + constants.token)
+      return $http.get(constants.production + 'emotions?cloud=true&lang_code=en&token=' + constants.token)
     }
   }])
 };
